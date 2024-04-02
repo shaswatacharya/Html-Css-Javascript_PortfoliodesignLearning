1a74f58505a91e1ac1bc23e5e282f7cd6552f00c
Shaswat Acharya <shaswatacharya123@gmail.com>
2024-04-02 19:40:16 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function toggleMenu(){
+    
     const menu = document.querySelector(".menu-links");
+    
     const icon = document.querySelector(".mobile-icon");
+    
     menu.classList.toggle("open");
+    
     icon.classList.toggle("open");
+    
 }
