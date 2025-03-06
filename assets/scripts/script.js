dce38f93134b97e80c1fe361d5ecf52e6f761152
Shaswat Acharya <shaswatacharya123@gmail.com>
2025-03-06 16:41:38 +0545

    dark mode: Added toggle feature
    Implemented a dark mode toggle in the portfolio. User preferences are saved in local storage for a consistent experience. Adjusted styles for better dark theme support.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+//for icons
+
 function toggleMenu(){
     
     const menu = document.querySelector(".menu-links");
@@ -9,3 +11,58 @@ function toggleMenu(){
     icon.classList.toggle("open");
     
 }
+
+
+// Mode Switch
+
+const btn = document.getElementById("modeToggle");
+const btn2 = document.getElementById("modeToggle2");
+const themeIcons = document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+    setDarkMode();
+  }
+  
+  btn.addEventListener("click", function () {
+    setTheme();
+  });
+  
+  btn2.addEventListener("click", function () {
+    setTheme();
+  });
+
+
+  
+  function setTheme() {
+    let currentTheme = document.body.getAttribute("theme");
+  
+    if (currentTheme === "dark") {
+      setLightMode();
+    } else {
+      setDarkMode();
+    }
+  }
+
+  //dark mode logic
+  function setDarkMode() {
+    document.body.setAttribute("theme", "dark");
+    localStorage.setItem("theme", "dark");
+  
+    themeIcons.forEach((icon) => {
+      icon.src = icon.getAttribute("src-dark");
+    });
+  }
+
+
+  //light mode logic
+
+
+  function setLightMode() {
+    document.body.removeAttribute("theme");
+    localStorage.setItem("theme", "light");
+  
+    themeIcons.forEach((icon) => {
+      icon.src = icon.getAttribute("src-light");
+    });
+  }
